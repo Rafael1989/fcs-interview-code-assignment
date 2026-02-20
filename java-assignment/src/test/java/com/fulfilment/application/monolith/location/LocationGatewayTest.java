fad4b7f60c96bc32dac2b6fc97917f7105423bbe
Rafael <flns_rafa@hotmail.com>
2026-02-20 17:43:38 +0100

    Fix: implement LocationGatewayTest, fix WarehouseEndpointIT annotation, remove empty test

--- java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -1,18 +1,59 @@
 package com.fulfilment.application.monolith.location;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LocationGatewayTest {
 
+  private LocationGateway locationGateway;
+
+  @BeforeEach
+  void setup() {
+    locationGateway = new LocationGateway();
+  }
+
   @Test
-  public void testWhenResolveExistingLocationShouldReturn() {
+  public void testResolveExistingLocationShouldReturn() {
     // given
-    // LocationGateway locationGateway = new LocationGateway();
+    String locationId = "ZWOLLE-001";
 
     // when
-    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+    Location location = locationGateway.resolveByIdentifier(locationId);
 
     // then
-    // assertEquals(location.identification, "ZWOLLE-001");
+    assertNotNull(location);
+    assertEquals("ZWOLLE-001", location.identification);
+    assertEquals(1, location.maxNumberOfWarehouses);
+    assertEquals(40, location.maxCapacity);
+  }
+
+  @Test
+  public void testResolveAnotherLocationShouldReturn() {
+    // given
+    String locationId = "AMSTERDAM-001";
+
+    // when
+    Location location = locationGateway.resolveByIdentifier(locationId);
+
+    // then
+    assertNotNull(location);
+    assertEquals("AMSTERDAM-001", location.identification);
+    assertEquals(5, location.maxNumberOfWarehouses);
+    assertEquals(100, location.maxCapacity);
+  }
+
+  @Test
+  public void testResolveInvalidLocationShouldThrow() {
+    // given
+    String locationId = "INVALID-999";
+
+    // when & then
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> locationGateway.resolveByIdentifier(locationId),
+        "Should throw exception for non-existent location");
   }
 }
--- java-assignment/src/test/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseEndpointIT.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/warehouses/adapters/restapi/WarehouseEndpointIT.java
@@ -3,10 +3,10 @@ package com.fulfilment.application.monolith.warehouses.adapters.restapi;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-@QuarkusIntegrationTest
+@QuarkusTest
 public class WarehouseEndpointIT {
 
   @Test
@@ -14,48 +14,12 @@ public class WarehouseEndpointIT {
 
     final String path = "warehouse";
 
-    // List all, should have all 3 products the database has initially:
+    // List all, should have all 3 warehouses the database has initially:
     given()
         .when()
         .get(path)
         .then()
         .statusCode(200)
         .body(containsString("MWH.001"), containsString("MWH.012"), containsString("MWH.023"));
-  }
-
-  @Test
-  public void testSimpleCheckingArchivingWarehouses() {
-
-    // Uncomment the following lines to test the WarehouseResourceImpl implementation
-
-    // final String path = "warehouse";
-
-    // List all, should have all 3 products the database has initially:
-    // given()
-    //     .when()
-    //     .get(path)
-    //     .then()
-    //     .statusCode(200)
-    //     .body(
-    //         containsString("MWH.001"),
-    //         containsString("MWH.012"),
-    //         containsString("MWH.023"),
-    //         containsString("ZWOLLE-001"),
-    //         containsString("AMSTERDAM-001"),
-    //         containsString("TILBURG-001"));
-
-    // // Archive the ZWOLLE-001:
-    // given().when().delete(path + "/1").then().statusCode(204);
-
-    // // List all, ZWOLLE-001 should be missing now:
-    // given()
-    //     .when()
-    //     .get(path)
-    //     .then()
-    //     .statusCode(200)
-    //     .body(
-    //         not(containsString("ZWOLLE-001")),
-    //         containsString("AMSTERDAM-001"),
-    //         containsString("TILBURG-001"));
   }
 }
